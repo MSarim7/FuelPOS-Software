fe6d509adc5c7d25d3ac6a685c257ae7f9b0517c
MSarim7 <msarim245@gmail.com>
2025-11-22 22:04:46 +0500

    build errors fix

--- .next/static/chunks/app/admin/expenses/page-581954b3ef409a0a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[938,931,473,271,349,603,753],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/employer/dashboard/page-141fee5f8b5a5e04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[349,931,271,938,473,603,753],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
